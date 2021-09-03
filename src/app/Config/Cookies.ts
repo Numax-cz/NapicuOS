f65533a0b67f916c7f4ec09c0dc538ee9830657a
Marcel <numax.cz@protonmail.ch>
2021-09-03 18:34:06 +0200

    Odstraněn error flashingu

--- src/app/Config/Cookies.ts
+++ src/app/Config/Cookies.ts
@@ -6,7 +6,3 @@ export const cookiesTime: number = 7 * 24 * 60 * 60 * 1000;
  * Default string cookies for bios settings
  */
 export const cookiesForBisoSettingsAr: string = 'BiosSettingsArray';
-/**
- * Cookies for possible fatal bios failure
- */
-export const cookiesForFlashingFatalERROR: string = 'FlashingFatalERROR';
