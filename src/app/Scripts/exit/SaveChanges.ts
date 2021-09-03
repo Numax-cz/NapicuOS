33af531133c320dc52076b372eb454de70176e7a
Marcel <numax.cz@protonmail.ch>
2021-09-03 18:30:56 +0200

    Uprava

--- src/app/Scripts/exit/SaveChanges.ts
+++ src/app/Scripts/exit/SaveChanges.ts
@@ -1,6 +1,5 @@
 import { BiosSettings } from 'src/app/Array/ToolSettings';
 import { BiosComponent } from 'src/app/Bios/bios/bios.component';
-import { BootComponent } from 'src/app/Bios/boot/boot.component';
 import { cookiesForBisoSettingsAr } from 'src/app/Config/Cookies';
 import { setCookies } from '../Cookies';
 import { Reboot } from './Reboot';
