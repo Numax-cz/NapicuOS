2fbf9e643fe4df82bdf08fa9b570aa109c314d9d
Marcel <numax.cz@protonmail.ch>
2021-09-04 10:19:51 +0200

    Informace o monitorech

--- src/app/Bios/monitor/monitor.component.ts
+++ src/app/Bios/monitor/monitor.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { MonitorInfo } from 'src/app/Array/Informations';
+import { Informations } from 'src/app/interface/Informations';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
-  styleUrls: ['./monitor.component.scss']
+  styleUrls: ['./monitor.component.scss'],
 })
 export class MonitorComponent implements OnInit {
+  public MainOptionInfo: Informations[] = MonitorInfo;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void { }
+  
 }
