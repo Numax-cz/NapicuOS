b22c79cb60faeefbda9df45ff4327e1f80b4b51b
Marcel <numax.cz@protonmail.ch>
2021-09-11 21:52:09 +0200

    Přidán Drives.ts

---
+++ src/app/Array/Drives.ts
@@ -0,0 +1,18 @@
+import { Options } from "../interface/ToolSettings";
+//TODO
+//? Samsung - Main System 
+//? IBM - Low graphic interface operating system
+//? Segate - No operating system
+
+
+export const drive: Options[] = [
+  {
+    title: '1. Samsung 860 EVO M.2 1TB',
+  },
+  {
+    title: '2. IBM 1405 0.0075 GB',
+  },
+  {
+    title: '3. Seagate ST-225 0.021 GB',
+  },
+];
