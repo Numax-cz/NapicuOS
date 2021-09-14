8dbc882c33a9deb6059e8002e2bafa1ee92efd86
Marcel <numax.cz@protonmail.ch>
2021-09-14 17:53:49 +0200

    Přidán text

---
+++ src/app/Config/BlackScreenTexts.ts
@@ -0,0 +1,8 @@
+export const NoBootDevice: string[] = [
+  'No boot device available.',
+  'Current boot mode is set to BIOS.',
+  'Please ensure compatible bootable media is available.',
+  'Use the system setup program to change the boot mode as needed.',
+  '',
+  'Press F1 to retry boot.',
+];
