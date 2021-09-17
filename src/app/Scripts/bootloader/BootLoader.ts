a4c6579b7375d40f302ffa96f2f8b70da55d8f49
Marcel <numax.cz@protonmail.ch>
2021-09-17 15:35:50 +0200

    Přejmenována funkce

---
+++ src/app/Scripts/bootloader/BootLoader.ts
@@ -0,0 +1,32 @@
+import { drive } from 'src/app/Array/Drives';
+import { BiosSettings } from 'src/app/Array/ToolSettings';
+import { NoBootDevice } from 'src/app/Config/BlackScreenTexts';
+import { setTime } from '../TimeDate';
+import { animationCursor, setDisplayText } from './text';
+/**
+ * Checks the disk to see if there is something to boot from
+ */
+export function BootLoader(): void {
+  var selected: number = BiosSettings.Boot.settings.boot_priority.selected;
+  let test = new animationCursor();
+  test.blinking();
+
+
+  setTimeout(() => {
+    if (checkBootSector(selected)) {
+    }
+  }, 1350);
+}
+
+function checkBootSector(selected: number): boolean {
+  if (drive && drive.length && drive[selected].data) {
+    if (!drive[selected].data.boot) {
+      setDisplayText(NoBootDevice);
+      return false;
+    }
+    return true;
+  } else {
+    //TODO No drives found
+    return false;
+  }
+}
