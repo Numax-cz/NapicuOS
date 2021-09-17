f108dc5ac51ce81d925a26a6e9c0a08bab68590f
Marcel <numax.cz@protonmail.ch>
2021-09-17 15:58:10 +0200

    Upraveno bootovací načítání

--- src/app/Scripts/bootloader/BootLoader.ts
+++ src/app/Scripts/bootloader/BootLoader.ts
@@ -1,16 +1,17 @@
 import { drive } from 'src/app/Array/Drives';
 import { BiosSettings } from 'src/app/Array/ToolSettings';
 import { NoBootDevice } from 'src/app/Config/BlackScreenTexts';
+import { Navigate } from '../BiosRouter';
 import { setTime } from '../TimeDate';
 import { animationCursor, setDisplayText } from './text';
 /**
  * Checks the disk to see if there is something to boot from
  */
 export function BootLoader(): void {
+  Navigate('/blackscreen');
   var selected: number = BiosSettings.Boot.settings.boot_priority.selected;
   let test = new animationCursor();
   test.blinking();
-
 
   setTimeout(() => {
     if (checkBootSector(selected)) {
--- src/app/Scripts/exit/Reboot.ts
+++ src/app/Scripts/exit/Reboot.ts
@@ -1,6 +1,7 @@
 import { BootComponent } from 'src/app/Bios/boot/boot.component';
 import { startTimeIn, startTimeOut } from 'src/app/Config/Animation/Boot';
 import { Navigate } from '../BiosRouter';
+import { BootLoader } from '../bootloader/BootLoader';
 import { Loading } from '../LoadingAnimations';
 
 export function Reboot(): void {
@@ -9,5 +10,5 @@ export function Reboot(): void {
 
 export function Continue(): void {
   BootComponent.EnterBios = false;
-  Navigate('/blackscreen'); 
+  BootLoader();
 }
