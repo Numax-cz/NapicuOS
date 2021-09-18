56ab9c079eb6d18c2ebf34a34584ef7d6a0a8eae
Marcel <numax.cz@protonmail.ch>
2021-09-18 17:22:38 +0200

    Odstraněny komentáře

--- src/app/Scripts/bootloader/BootLoader.ts
+++ src/app/Scripts/bootloader/BootLoader.ts
@@ -5,42 +5,24 @@ import { NoBootDevice } from 'src/app/Config/BlackScreenTexts';
 import { Navigate } from '../BiosRouter';
 import { setTime } from '../TimeDate';
 import { animationCursor, setDisplayText } from './text';
+
 /**
- * Checks the disk to see if there is something to boot from
+ * Main part of the system bootloader
  */
-export function BootLoaderTEST(): void {
-  //   Navigate('/blackscreen');
-  //   var selected: number = BiosSettings.Boot.settings.boot_priority.selected;
-  //   let test = new animationCursor();
-  //   test.blinking();
-  //   setTimeout(() => {
-  //     if (checkBootSector(selected)) {
-  //     }
-  //   }, 1350);
-}
-
-// function checkBootSector(selected: number): boolean {
-//   if (drive && drive.length && drive[selected].data) {
-//     if (!drive[selected].data.boot) {
-//       setDisplayText(NoBootDevice);
-//       return false;
-//     }
-//     return true;
-//   } else {
-//     //TODO No drives found
-//     return false;
-//   }
-// }
-
 export class BootLoader {
   protected selectedBootPriority: number;
   protected biosConfig: BiosOptionsST;
-
+  /**
+   *
+   * @param biosConfig - Bios Settings
+   */
   constructor(biosConfig: BiosOptionsST) {
     this.biosConfig = biosConfig;
     this.selectedBootPriority = this.biosConfig.Boot.settings.boot_priority.selected;
   }
-
+  /**
+   * Checks all necessary parts of the setup
+   */
   public check(): void {
     BlackscreenComponent.cursor = new animationCursor();
     BlackscreenComponent.cursor.blinking();
@@ -50,6 +32,9 @@ export class BootLoader {
     }, 1350);
   }
 
+  /**
+   * Checks the disk to see if there is something to boot from
+   */
   protected checkBootSector(): boolean {
     if (drive && drive.length && drive[this.selectedBootPriority].data) {
       if (!drive[this.selectedBootPriority].data.boot) {
