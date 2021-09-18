089a9c5e0b96f70a2c03a638168d06904205fe07
Marcel <numax.cz@protonmail.ch>
2021-09-18 21:24:58 +0200

    Přidána funkce na vrácení vybraného itemu

---
+++ src/app/Scripts/getSelectedItem.ts
@@ -0,0 +1,12 @@
+import { SettingsTemplateComponent } from '../Bios/settings-template/settings-template.component';
+import { settings } from '../interface/ToolSettings';
+import { objectKeys } from './objectKeys';
+/**
+ * 
+ * @returns Returns the selected item
+ */
+export function getSelectedItem(): settings {
+  return SettingsTemplateComponent.MainOption[
+    objectKeys(SettingsTemplateComponent.MainOption)[SettingsTemplateComponent.selected]
+  ];
+}
