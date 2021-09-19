288acd3fb7a472eb5380864c8c3706f7a6c83032
Marcel <numax.cz@protonmail.ch>
2021-09-19 16:18:51 +0200

    Zavaděč systému převeden do funkce

--- src/app/Scripts/bootloader/BootLoader.ts
+++ src/app/Scripts/bootloader/BootLoader.ts
@@ -2,6 +2,7 @@ import { drive } from 'src/app/Array/Drives';
 import { BiosOptionsST, BiosSettings } from 'src/app/Array/ToolSettings';
 import { BlackscreenComponent } from 'src/app/Bios/blackscreen/blackscreen.component';
 import { NoBootDevice } from 'src/app/Config/BlackScreenTexts';
+import { LoadsComponent } from 'src/app/System/loads/loads.component';
 import { copy } from '../DeepClone';
 import { Loading } from '../LoadingAnimations';
 import { animationCursor, setDisplayText } from './text';
@@ -28,18 +29,26 @@ export class BootLoader {
     BlackscreenComponent.cursor.blinking();
     setTimeout(() => {
       if (this.checkBootSector()) {
-        setTimeout(() => {
-          BlackscreenComponent.cursor?.moveDown(() => {
-            Loading('/booting', 500, 1050);
-          });
-        }, 1230);
+        this.system();
       }
     }, 1350);
   }
   /**
    * Functions to continue to the system
    */
-  protected system(): void {}
+  protected system(): void {
+    var system = drive[this.selectedBootPriority].data.system;
+    if (system) {
+      LoadsComponent.Systems = system;
+      setTimeout(() => {
+        BlackscreenComponent.cursor?.moveDown(() => {
+          Loading('/booting', 500, 1050);
+        });
+      }, 1230);
+    } else {
+      //TODO Error system boot
+    }
+  }
   /**
    * Checks the disk to see if there is something to boot from
    */
