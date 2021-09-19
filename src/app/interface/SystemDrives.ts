4623eaf2186eea2713981978ac25a8f5148edd87
Marcel <numax.cz@protonmail.ch>
2021-09-19 15:11:49 +0200

    Upraveny interfaci

--- src/app/interface/SystemDrives.ts
+++ src/app/interface/SystemDrives.ts
@@ -1,8 +1,12 @@
+import { system } from "./System";
+
 export interface SystemDrives {
   title: string;
   data: SystemDrivesData; //TODO add ? 
 }
 //TODO Add more items
 export interface SystemDrivesData{
-  boot?: any
+  boot?: {
+    system?: system[]
+  }
 }
