4137a9407762dc1ba23988e65195fd397f6ef536
Marcel <numax.cz@protonmail.ch>
2021-09-26 10:51:37 +0200

    Uprava

--- src/app/Sys/Systems/NapicuOS/napicu-os/napicu-os.component.ts
+++ src/app/Sys/Systems/NapicuOS/napicu-os/napicu-os.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
   selector: 'app-napicu-os',
   templateUrl: './napicu-os.component.html',
-  styleUrls: ['./napicu-os.component.scss']
+  styleUrls: ['./napicu-os.component.scss'],
 })
 export class NapicuOSComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
