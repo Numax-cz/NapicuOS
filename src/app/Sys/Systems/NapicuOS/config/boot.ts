98f84c34960cd467d99ef2e8d5cb728b4908931e
Marcel <numax.cz@protonmail.ch>
2021-09-28 09:29:47 +0200

    Přidána dokumentace

--- src/app/Sys/Systems/NapicuOS/config/boot.ts
+++ src/app/Sys/Systems/NapicuOS/config/boot.ts
@@ -1,2 +1,8 @@
+/**
+ * time of displaying system logo
+ */
 export const boot_time: number = 0; //TODO 9250
+/**
+ * Delay between booting and system display
+ */
 export const soft_boot_time: number = 0; //TODO 1200
