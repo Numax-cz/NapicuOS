006bc6a6f4d9372646a78b7b8c1286c7be76cc81
Marcel <numax.cz@protonmail.ch>
2021-10-02 16:48:41 +0200

    Přidán boot_animation_time

--- src/app/Sys/Systems/NapicuOS/config/boot.ts
+++ src/app/Sys/Systems/NapicuOS/config/boot.ts
@@ -1,8 +1,14 @@
 /**
  * Time of displaying system logo
  */
-export const boot_time: number = 9250; //TODO 9250
+
+export const boot_time: number = 0; //TODO 9250
 /**
  * Delay between booting and system display
  */
-export const soft_boot_time: number = 1200; //TODO 1200
+export const soft_boot_time: number = 0; //TODO 1200
+
+/**
+ * Time indicating the duration of the animation right after the start
+ */
+export const boot_animation_time = 2;
