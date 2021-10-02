62cf2e224bbe18e078ba4ee2539ac0c98f553f8a
Marcel <numax.cz@protonmail.ch>
2021-10-02 18:59:59 +0200

    Přesunut template component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { GrubComponent } from './System/grub/grub.component';
 import { SystemComponent } from './System/system/system.component';
 import { NapicuOSComponent } from './Sys/Systems/NapicuOS/components/napicu-os/napicu-os.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WindowComponent } from './Sys/template/window/window.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -43,7 +43,7 @@ import { WindowComponent } from './Sys/template/window/window.component';
     GrubComponent,
     SystemComponent,
     NapicuOSComponent,
-    WindowComponent,
+
   ],
   imports: [
     BrowserModule,
