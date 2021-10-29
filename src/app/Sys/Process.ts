f553e27d0a5aef830c3b1aa9a22dbb3b5aee8c64
Marcel <numax.cz@protonmail.ch>
2021-10-29 09:59:57 +0200

    Upraveny okna

--- src/app/Sys/Process.ts
+++ src/app/Sys/Process.ts
@@ -8,11 +8,19 @@ interface Inter {
 export class Process {
   public title: string = 'NapicuAPP';
   public declare Interval: any;
+  public declare display: boolean;
   public declare Window: Window;
   public declare component: Type<any>;
   public readonly run = (): void => {};
   public readonly close = (): void => {};
-  constructor(data?: { Window?: Window }) {}
+  constructor(data?: { Window?: Window }) {
+    if (data?.Window) this.Window = data.Window;
+  }
+
+  public readonly open = (): void => {
+    this.display = true;
+  };
+
   public onRun(): void {}
 
   public onClose(): void {}
