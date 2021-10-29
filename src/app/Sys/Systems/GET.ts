f98b585d82050786cf6bf0bd6a6e11b3693ea459
Marcel <numax.cz@protonmail.ch>
2021-10-29 09:59:40 +0200

    Přidán GET

--- src/app/Sys/Systems/GET.ts
+++ src/app/Sys/Systems/GET.ts
@@ -1,5 +1,11 @@
-import { NapicuOS } from "./NapicuOS/system.napicuos";
+import { GrubComponent } from 'src/app/System/grub/grub.component';
+import { NapicuOS } from './NapicuOS/system.napicuos';
 
-export function getSystemTime(): string { //TODO string/number
-    return NapicuOS.systemTime;
+export function getSystemTime(): string {
+  //TODO string/number
+  return NapicuOS.systemTime;
+}
+
+export function SystemBoot(): boolean {
+  return GrubComponent.ActiveSystem.SystemBooted;
 }
