aa1b16d7835ec8a85efc9d668452081828d1b4e5
Marcel <numax.cz@protonmail.ch>
2021-10-29 10:07:27 +0200

    Přidána dokumentace

--- src/app/Sys/Systems/GET.ts
+++ src/app/Sys/Systems/GET.ts
@@ -1,11 +1,15 @@
 import { GrubComponent } from 'src/app/System/grub/grub.component';
 import { NapicuOS } from './NapicuOS/system.napicuos';
-
+/**
+ * Returns system time
+ */
 export function getSystemTime(): string {
-  //TODO string/number
   return NapicuOS.systemTime;
 }
 
+/**
+ * Returns whether the system has been started
+ */
 export function SystemBoot(): boolean {
   return GrubComponent.ActiveSystem.SystemBooted;
 }
