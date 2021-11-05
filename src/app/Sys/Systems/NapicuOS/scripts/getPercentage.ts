e485a630bb9f27bbf5b447b343dc9743092a6f36
Marcel <numax.cz@protonmail.ch>
2021-11-05 19:18:40 +0100

    Přidána funkce pro výpočet hodnoty z %

--- src/app/Sys/Systems/NapicuOS/scripts/getPercentage.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/getPercentage.ts
@@ -1,3 +1,7 @@
 export function percentage(partialValue: number, totalValue: number) {
   return (100 * partialValue) / totalValue;
 }
+
+export function percentageValue(percentage: number, totalValue: number) {
+  return (totalValue / 100) * percentage;
+}
