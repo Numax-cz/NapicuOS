2e9b4832772a7301a0d92305c4feb919bd77dfaf
Marcel <numax.cz@protonmail.ch>
2021-11-11 18:20:22 +0100

    Složka přesunuta do hlavního systémového adresáře

---
+++ src/app/Sys/Systems/NapicuOS/interface/WindowStatus..d.ts
@@ -0,0 +1,5 @@
+export declare type minimized = 'minimized';
+export declare type maximized = 'maximized';
+export declare type closed = 'closed';
+export declare type open = 'oped';
+export declare type WindowStatus = minimized | maximized | closed | open;
