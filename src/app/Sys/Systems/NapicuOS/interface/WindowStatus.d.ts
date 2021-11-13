90d3f897b6cfc6bc3723aad3cb356c6dc749ef0e
Marcel <numax.cz@protonmail.ch>
2021-11-13 10:00:38 +0100

    Smazán deklarační soubor

--- src/app/Sys/Systems/NapicuOS/interface/WindowStatus.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type minimized = 'minimized';
-export declare type maximized = 'maximized';
-export declare type closed = 'closed';
-export declare type open = 'opened';
-export declare type WindowStatus = minimized | maximized | closed | open;
