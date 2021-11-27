a79e724bb2a41091787e6e7638347e6d9d20b0d1
Marcel <numax.cz@protonmail.ch>
2021-11-27 16:14:35 +0100

    Přidán interface classy aplikačního okna

---
+++ src/app/Sys/Systems/NapicuOS/interface/Window/windowData.d.ts
@@ -0,0 +1,26 @@
+export declare interface windowData {
+  /**
+   * Window position in X (value in percentage)
+   */
+  posX: number;
+  /**
+   * Window position in X (value in percentage)
+   */
+  posY: number;
+  /**
+   * Window width
+   */
+  width: number;
+  /**
+   * Window height
+   */
+  height: number;
+  /**
+   * Window z-index
+   */
+  z_index: number;
+  /**
+   * Specifies whether the window is maximized
+   */
+  maximized: boolean;
+}
