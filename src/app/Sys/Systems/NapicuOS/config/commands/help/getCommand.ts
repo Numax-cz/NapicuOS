2c7e2ada1255a49c80b73f516af9e055a6c31881
Marcel <numax.cz@protonmail.ch>
2022-01-16 13:29:36 +0100

    Přidán config pro jednotlivé příkazy

---
+++ src/app/Sys/Systems/NapicuOS/config/commands/help/getCommand.ts
@@ -0,0 +1,14 @@
+import { Line } from '../../../Apps/console/console.component';
+import { helpCommandTemplate } from '../../../initCommands.napicuos';
+
+export const getHelpCommandAPPS: Line = new Line(
+  `\t\t--open - Returns open GUI applications
+    \t\t--close - Returns close GUI applications`
+);
+
+export const getHelpCommand: Line = new Line(
+    `Options:
+    systemprocess - Returns system processes running in the background
+    commands - Returns available commands
+    apps - Returns running GUI applications
+    ${getHelpCommandAPPS.Write().value}`);
