85a1741e02a256f9616c6b3fed06a9231f70398a
Marcel <numax.cz@protonmail.ch>
2022-01-22 19:00:37 +0100

    Přidána animace logout

--- src/app/Sys/Systems/NapicuOS/components/loginscreen/loginscreen.component.ts
+++ src/app/Sys/Systems/NapicuOS/components/loginscreen/loginscreen.component.ts
@@ -1,10 +1,25 @@
+import { trigger, transition, query, stagger, style, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { log_screen_animation_time } from '../../config/logscreen';
 import { wallpaper } from '../../config/wallpaper';
 
 @Component({
   selector: 'app-loginscreen',
   templateUrl: './loginscreen.component.html',
   styleUrls: ['./loginscreen.component.scss', '../napicu-os/napicu-os.component.scss'],
+  animations: [
+    trigger('NapicuOSfeoreLoginScreen', [
+      transition('* => *', [
+        query(
+          ':self',
+          stagger('20ms', [
+            style({ transform: 'translateY(-75%)', opacity: 0, transformOrigin: 'bottom' }),
+            animate(`${log_screen_animation_time}ms ease-in-out`),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class LoginscreenComponent implements OnInit {
   constructor() {}
---
+++ src/app/Sys/Systems/NapicuOS/config/logscreen.ts
@@ -0,0 +1,4 @@
+/**
+ * Time indicating the duration of the login window display
+ */
+export const log_screen_animation_time = 750;
