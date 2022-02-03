62936f0a5d1b0ff3ddadf5be4c37fb2e885b0090
Marcel <numax.cz@protonmail.ch>
2022-02-03 20:35:56 +0100

    Upraven Welcome component

--- src/app/Sys/Systems/NapicuOS/Apps/welcome/welcome.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/welcome/welcome.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+
+
+interface barMetadata {
+  value: string,
+  template: TemplateRef<any>
+}
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent implements OnInit {
-  constructor() {}
+export class WelcomeComponent implements OnInit, AfterViewInit {
+  @ViewChild('Welcome') public declare Welcome: TemplateRef<any>;
 
-  ngOnInit(): void {}
+  protected barOptions: barMetadata[] = [];
+  protected selectedBarOption: number = 0;
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.Welcome)
+    this.barOptions = [{value: "Welcome", template: this.Welcome}]
+  }
+
+  get GetBarOptions(): barMetadata[] {
+    return this.barOptions
+  }
+
+  get GetSelectedBarOption(): number {
+    return this.selectedBarOption
+  }
+
+
 }
