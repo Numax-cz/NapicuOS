dab3f66989bd5e7726f689ad8a9c7cf429a43863
Marcel <numax.cz@protonmail.ch>
2022-02-05 16:30:31 +0100

    Přidán soubor pro nastavení prefixů systémových příkazů

---
+++ src/app/Sys/Systems/NapicuOS/interface/Commands/commands.d.ts
@@ -0,0 +1,10 @@
+export const enum SystemCommandsPrefixEnum {
+  shellCommand = "shell",
+  addUserCommand = "adduser",
+  getCommand = "get",
+  setCommand = "set",
+  killCommand = "kill",
+  logoutCommand = "logout",
+  exitCommand = "exit",
+  openAppCommand = "openapp",
+}
