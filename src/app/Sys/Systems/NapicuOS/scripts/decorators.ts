e25352c146cbd079dc4bf3a81f8ca06b651a0fe3
Marcel <numax.cz@protonmail.ch>
2022-03-14 17:35:44 +0100

    Vymazán nepotřebný decorator

--- src/app/Sys/Systems/NapicuOS/scripts/decorators.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/decorators.ts
@@ -1,27 +1,5 @@
 import {NapicuOS} from "../system.napicuos";
 
-export function NapicuDock() {
-  return function (target: any, key: string | symbol) {
-    let p = target[key];
-    const getter = () => {
-      return p;
-    };
-
-    const setter = (next: any) => {
-      p = next;
-      NapicuOS.update_dock_items();
-      return p;
-    };
-
-    Object.defineProperty(target, key, {
-      get: getter,
-      set: setter,
-      enumerable: true,
-      configurable: true,
-    });
-  };
-}
-
 export function NapicuCookies() {
   return function (target: any, key: string | symbol) {
     let p = target[key];
