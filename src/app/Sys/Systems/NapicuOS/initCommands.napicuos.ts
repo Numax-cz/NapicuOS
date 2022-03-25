9c5022eefd5f71d159ec98cc4bf607cdc95ed0e7
Marcel <numax.cz@protonmail.ch>
2022-03-25 20:59:20 +0100

    Upraven KillProcess příkaz

--- src/app/Sys/Systems/NapicuOS/initCommands.napicuos.ts
+++ src/app/Sys/Systems/NapicuOS/initCommands.napicuos.ts
@@ -261,17 +261,18 @@ function initKillProcess(): void {
     new Command('SystemProcessKiller', SystemCommandsPrefixEnum.killCommand, (params) => {
       return new Promise((resolve) => {
         if (params?.length) {
-          if (params[0]) {
-            let x = NapicuOS.get_system_process_by_title(params[0]);
+          let pid = params[0]
+          if (pid) {
+            let x = NapicuOS.get_system_process_by_pid(Number(pid));
             if (x) resolve(x.kill());
             resolve({
-              linesForCMD: [new Line(`Process '${params[0]}' not found`)],
+              linesForCMD: [new Line(`Process with pid'${params[0]}' not found`)],
               stateCode: CommandStateCodeMetadata.ProcessNotFound,
             });
           }
         } else {
           resolve({
-            linesForCMD: [usageCommand(`kill <process_name>`)],
+            linesForCMD: [usageCommand(`kill <pid>`)],
             stateCode: CommandStateCodeMetadata.success,
           });
         }
