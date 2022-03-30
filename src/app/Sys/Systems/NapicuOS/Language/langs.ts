45b3598b6db3c769f4bc6704ad9091854a2e9689
Marcel <numax.cz@protonmail.ch>
2022-03-30 17:50:54 +0200

    Přidány další překlady

--- src/app/Sys/Systems/NapicuOS/Language/langs.ts
+++ src/app/Sys/Systems/NapicuOS/Language/langs.ts
@@ -1,5 +1,23 @@
-export const NapicuOSLanguages = {
-  en: {},
+import {lang_Calendar_en, lang_Calendar_cs, lang_Notification_en, lang_Notification_cs} from "./CalendarMenu";
 
-  cs: {}
+export const NapicuOSLanguages = {
+  en: {
+    Sys: {
+      activities: "Activities",
+    },
+    CalendarMenu: {
+      Notification: lang_Notification_en,
+      Calendar: lang_Calendar_en
+    }
+  },
+
+  cs: {
+    Sys: {
+      activities: "Aktivity",
+    },
+    CalendarMenu: {
+      Notification: lang_Notification_cs,
+      Calendar: lang_Calendar_cs
+    }
+  }
 }
