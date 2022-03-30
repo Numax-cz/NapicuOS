d5198468b44861166cbec39b2bde08ad96feac7c
Marcel <numax.cz@protonmail.ch>
2022-03-30 17:50:47 +0200

    Přidány další překlady, přejmenován hlavní soubor

---
+++ src/app/Sys/Systems/NapicuOS/Language/CalendarMenu.ts
@@ -0,0 +1,38 @@
+export const lang_Calendar_en = {
+  days: [
+    'Su',
+    'Mo',
+    'Tu',
+    'We',
+    'Th',
+    'Fr',
+    'Sa'
+  ],
+  next: "Next",
+  previous: "Previous",
+}
+
+export const lang_Calendar_cs = {
+  days: [
+    'Po',
+    'Út',
+    'St',
+    'Čt',
+    'Pá',
+    'So',
+    'Ne',
+  ],
+  next: 'Další',
+  previous: 'Předchozí',
+}
+
+export const lang_Notification_en = {
+  title: 'Notification',
+  notNotification: 'No notifications',
+  doNotDisturb: 'Do not disturb',
+}
+export const lang_Notification_cs = {
+  title: 'Upozornění',
+  notNotification: 'Žádná upozornění',
+  doNotDisturb: 'Nerušit',
+}
