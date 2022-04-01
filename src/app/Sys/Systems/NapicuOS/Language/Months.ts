df4ed581112aeff29b8c049e5b6709757f41062e
Marcel <numax.cz@protonmail.ch>
2022-04-01 18:33:38 +0200

    Přidán překlad pro měsíce

---
+++ src/app/Sys/Systems/NapicuOS/Language/Months.ts
@@ -0,0 +1,29 @@
+export const lang_Month_en = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+export const lang_Month_cs = [
+  "Leden",
+  "Únor",
+  "Březen",
+  "Duben",
+  "Květen",
+  "Červen",
+  "Červenec",
+  "Srpen",
+  "Září",
+  "Říjen",
+  "Listopad",
+  "Prosinec"
+];
