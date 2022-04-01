73e699ce56a32acca983eb9c694c28ab044b7c7a
Marcel <numax.cz@protonmail.ch>
2022-04-01 18:34:07 +0200

    Přidán překlad pro měsíce

--- src/app/Sys/Systems/NapicuOS/Language/langs.ts
+++ src/app/Sys/Systems/NapicuOS/Language/langs.ts
@@ -1,4 +1,5 @@
 import {lang_Calendar_en, lang_Calendar_cs, lang_Notification_en, lang_Notification_cs} from "./CalendarMenu";
+import {lang_Month_cs, lang_Month_en} from "./Months";
 
 export declare type NapicuOS_available_language = "en" | "cs";
 
@@ -10,7 +11,8 @@ export const NapicuOSLanguages = {
     CalendarMenu: {
       Notification: lang_Notification_en,
       Calendar: lang_Calendar_en
-    }
+    },
+    Months: lang_Month_en
   },
 
   cs: {
@@ -20,6 +22,7 @@ export const NapicuOSLanguages = {
     CalendarMenu: {
       Notification: lang_Notification_cs,
       Calendar: lang_Calendar_cs
-    }
+    },
+    Months: lang_Month_cs
   }
 }
