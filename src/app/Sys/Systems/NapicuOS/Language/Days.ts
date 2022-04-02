6825f20f8cca402154289a6a35e47d371ff698e1
Marcel <numax.cz@protonmail.ch>
2022-04-02 11:35:22 +0200

    Přidán překlad dní

---
+++ src/app/Sys/Systems/NapicuOS/Language/Days.ts
@@ -0,0 +1,19 @@
+export const lang_Days_en = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export const lang_Days_cs = [
+  "Pondělí",
+  "Úterý",
+  "Středa",
+  "Čtvrtek",
+  "Pátek",
+  "Sobota",
+  "Neděle",
+];
--- src/app/Sys/Systems/NapicuOS/Language/langs.ts
+++ src/app/Sys/Systems/NapicuOS/Language/langs.ts
@@ -1,5 +1,6 @@
 import {lang_Calendar_en, lang_Calendar_cs, lang_Notification_en, lang_Notification_cs} from "./CalendarMenu";
 import {lang_Month_cs, lang_Month_en} from "./Months";
+import {lang_Days_cs, lang_Days_en} from "./Days";
 
 export declare type NapicuOS_available_language = "en" | "cs";
 
@@ -12,7 +13,8 @@ export const NapicuOSLanguages = {
       Notification: lang_Notification_en,
       Calendar: lang_Calendar_en
     },
-    Months: lang_Month_en
+    Months: lang_Month_en,
+    Days: lang_Days_en
   },
 
   cs: {
@@ -23,6 +25,7 @@ export const NapicuOSLanguages = {
       Notification: lang_Notification_cs,
       Calendar: lang_Calendar_cs
     },
-    Months: lang_Month_cs
+    Months: lang_Month_cs,
+    Days: lang_Days_cs
   }
 }
