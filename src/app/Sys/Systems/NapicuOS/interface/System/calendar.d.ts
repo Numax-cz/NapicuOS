de42614a43c3ad7630c8b8f4509e35cd6982c12e
Marcel <numax.cz@protonmail.ch>
2022-04-03 10:14:42 +0200

    Přidán interface pro systémový kalendář

---
+++ src/app/Sys/Systems/NapicuOS/interface/System/calendar.d.ts
@@ -0,0 +1,7 @@
+import {NapicuCalendarDateMetadata} from "../Calendar/calendar";
+
+export declare interface SystemCalendarMetadata {
+  calendar: NapicuCalendarDateMetadata[][];
+  calendarDays: string[];
+  selectedMonth: number;
+}
