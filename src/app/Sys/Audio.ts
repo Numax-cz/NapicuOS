1a1c5b5f9826cd3b02f57ffce9ee4fe8ccc15c98
Marcel <numax.cz@protonmail.ch>
2022-04-07 17:06:23 +0200

    Přidány složka audio pro spravování zvuku

---
+++ src/app/Sys/Audio.ts
@@ -0,0 +1,27 @@
+export class NapicuAudio {
+
+  /**
+   * The dir where the audio files are stored
+   */
+  protected readonly defaultSoundFolder: string = 'assets/sound/';
+  /**
+   * Variable of the audio context
+   */
+  protected declare readonly _audio: HTMLAudioElement;
+
+  /**
+   * @param src The path of the audio file
+   * @param gain The gain of the audio
+   */
+  constructor(src: string, gain?: number) {
+    this._audio = new Audio(this.defaultSoundFolder + src);
+    this._audio.volume = gain || 1;
+  }
+
+  /**
+   * Play the audio
+   */
+  public play(): void {
+    this._audio.play();
+  }
+}
