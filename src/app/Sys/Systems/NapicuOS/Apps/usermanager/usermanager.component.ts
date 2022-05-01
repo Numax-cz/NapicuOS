de752511aa8cb6ba0a217a33a68b9f48fad93cd3
Marcel <numax.cz@protonmail.ch>
2022-05-01 11:01:39 +0200

    Uprava inputů, uprava aplikace, uprava dropdownmenu

--- src/app/Sys/Systems/NapicuOS/Apps/usermanager/usermanager.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/usermanager/usermanager.component.ts
@@ -1,5 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {SystemUserPermissionsEnumMetadata} from "../../config/UserPerms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {
+  SYSTEM_HOSTNAME_MAX_LENGTH,
+  SYSTEM_HOSTNAME_MIN_LENGTH,
+  SYSTEM_USERS_MAX_LENGTH, SYSTEM_USERS_MAX_PASSWORD_LENGTH,
+  SYSTEM_USERS_MIN_LENGTH, SYSTEM_USERS_MIN_PASSWORD_LENGTH
+} from "../../config/System";
+import {NgFormCheckIfMatchingPasswords} from "../../scripts/NgFormMatchingPasswords";
 
 @Component({
   selector: 'app-usermanager',
@@ -10,6 +18,22 @@ export class UsermanagerComponent implements OnInit {
 
   constructor() {
   }
+
+
+  public formData = new FormGroup({
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(SYSTEM_USERS_MIN_LENGTH),
+      Validators.maxLength(SYSTEM_USERS_MAX_LENGTH),
+    ]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(SYSTEM_USERS_MIN_PASSWORD_LENGTH),
+      Validators.maxLength(SYSTEM_USERS_MAX_PASSWORD_LENGTH),
+    ]),
+
+  });
 
   ngOnInit(): void {
   }
