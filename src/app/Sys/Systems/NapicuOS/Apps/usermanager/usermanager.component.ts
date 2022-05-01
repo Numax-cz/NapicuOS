186f242aee3da48e9fbcf0a4258b0a6e71264f9c
Marcel <numax.cz@protonmail.ch>
2022-05-01 17:38:53 +0200

    Přidán form controller

--- src/app/Sys/Systems/NapicuOS/Apps/usermanager/usermanager.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/usermanager/usermanager.component.ts
@@ -1,20 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, forwardRef, OnInit} from '@angular/core';
 import {SystemUserPermissionsEnumMetadata} from "../../config/UserPerms";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ControlValueAccessor, FormControl, FormGroup, NG_VALUE_ACCESSOR, Validators} from "@angular/forms";
 import {
-  SYSTEM_HOSTNAME_MAX_LENGTH,
-  SYSTEM_HOSTNAME_MIN_LENGTH,
   SYSTEM_USERS_MAX_LENGTH, SYSTEM_USERS_MAX_PASSWORD_LENGTH,
   SYSTEM_USERS_MIN_LENGTH, SYSTEM_USERS_MIN_PASSWORD_LENGTH
 } from "../../config/System";
-import {NgFormCheckIfMatchingPasswords} from "../../scripts/NgFormMatchingPasswords";
+
 
 @Component({
   selector: 'app-usermanager',
   templateUrl: './usermanager.component.html',
   styleUrls: ['./usermanager.component.scss']
 })
-export class UsermanagerComponent implements OnInit {
+export class UsermanagerComponent {
 
   constructor() {
   }
@@ -33,13 +31,23 @@ export class UsermanagerComponent implements OnInit {
       Validators.maxLength(SYSTEM_USERS_MAX_PASSWORD_LENGTH),
     ]),
 
+    permission: new FormControl('', [
+      Validators.required,
+    ]),
   });
 
-  ngOnInit(): void {
+  public onButtonClick() {
+    // if(this.formData.valid){
+    //   let i = NapicuOS.add_user(new User({
+    //     username: this
+    //   }));
+    // }
+    console.log(this.formData.get("permission"))
   }
-
 
   get GetUsersPermission() {
     return Object.keys(SystemUserPermissionsEnumMetadata);
   }
+
+
 }
--- src/app/Sys/Systems/NapicuOS/template/drop-down-menu/drop-down-menu.component.ts
+++ src/app/Sys/Systems/NapicuOS/template/drop-down-menu/drop-down-menu.component.ts
@@ -1,4 +1,5 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, forwardRef, Host, Input, OnInit, Optional, Output, SkipSelf} from '@angular/core';
+import {AbstractControl, ControlContainer, ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 interface DropDownMenuData {
   name: string,
@@ -8,20 +9,54 @@ interface DropDownMenuData {
 @Component({
   selector: 'napicu-drop-down-menu',
   templateUrl: './drop-down-menu.component.html',
-  styleUrls: ['./drop-down-menu.component.scss']
+  styleUrls: ['./drop-down-menu.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: DropDownMenuComponent,
+    multi: true
+  }]
 })
-export class DropDownMenuComponent {
+export class DropDownMenuComponent implements ControlValueAccessor, OnInit {
   @Input() options: string[] = []
   @Input() selectedOption: number = 0;
   @Output() napicuDropDownMenuClick = new EventEmitter<string>();
+  @Input() declare formControlName: string;
+  private declare control: AbstractControl;
   public isDropDownOpen = false;
 
-  constructor() {
+  constructor(@Optional() @Host() @SkipSelf()
+              private controlContainer: ControlContainer) {
   }
 
 
   public selectItem(index: number): void {
     this.selectedOption = index;
     this.isDropDownOpen = false;
+    if (this.control) this.control.setValue(this.options[index]);
   }
+
+  ngOnInit(): void {
+    if (this.controlContainer) {
+      if (this.formControlName) {
+        let i = this.controlContainer.control?.get(this.formControlName);
+        if (i) this.control = i;
+        this.control.setValue(this.options[this.selectedOption]);
+      } else {
+        console.warn('FormControlName error');
+      }
+    } else {
+      console.warn('Can\'t find parent FormGroup directive');
+    }
+
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  writeValue(obj: any): void {
+  }
+
 }
