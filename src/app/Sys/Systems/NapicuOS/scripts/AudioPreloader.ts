8da0e2210caf09bfd5e56c6d36773be18addff93
Marcel <numax.cz@protonmail.ch>
2022-05-02 20:49:40 +0200

    Přidán audio preloader, uprava dokumentace

--- src/app/Sys/Systems/NapicuOS/scripts/AudioPreloader.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/AudioPreloader.ts
@@ -1,10 +1,10 @@
 /**
- * Function for preloading image
+ * Function for preloading audio
  * @param src
  */
 export function imagePreloader(src: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    const img = new Image();
+    const img = new Audio();
     img.onload = () => resolve();
     img.onerror = () => reject();
     img.src = src;
