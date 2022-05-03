b22a9f617ff4ce5aae455594ab1664ff0edfbcfa
Marcel <numax.cz@protonmail.ch>
2022-05-03 18:11:46 +0200

    Upraven loading

--- src/app/Sys/Systems/NapicuOS/components/loads/loads.component.ts
+++ src/app/Sys/Systems/NapicuOS/components/loads/loads.component.ts
@@ -1,26 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {GrubComponent} from 'src/app/Grub/grub/grub.component';
+import {animate, style, transition, trigger} from "@angular/animations";
+import {NOTIFICATION_ANIMATION_TIME} from "../../config/NotificationAnimations";
 
 @Component({
   selector: 'app-loads',
   templateUrl: './loads.component.html',
   styleUrls: ['./loads.component.scss'],
+  animations: [
+    trigger('NapicuOSfeoreLoading', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate(1400, style({opacity: 1})),
+      ]),
+
+    ]),
+  ]
 })
-export class LoadsComponent implements OnInit {
-  
+export class LoadsComponent {
 
   constructor() {
   }
 
 
-  ngOnInit(): void {
-  }
-
-  get Img(): string {
+  get GetImg(): string {
     let logo = GrubComponent.GrubActiveSystem.boot.logo;
     if (logo) {
       return logo;
     }
     return '';
+  }
+
+  get GetText(): string {
+    return GrubComponent.GrubActiveSystem.boot.title;
   }
 }
