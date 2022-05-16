50d42211ac99174877db3c263ddb20edc6894185
Marcel <numax.cz@protonmail.ch>
2022-05-16 15:30:11 +0200

    Uprava

--- src/app/Sys/Systems/NapicuOS/Apps/file/file.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/file/file.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {SYSTEM_DEFAULT_HOME_FOLDERS} from "../../config/System";
 import {fileConfigDisplayedMetadata, fileConfigMetadata} from "../../interface/Apps/FileManager";
 import {GET_SYSTEM_APPS_FILE_MANAGER_DISPLAYED_FILES} from "../../config/Apps/fileManager";
+import {NapicuOS} from "../../system.napicuos";
 
 @Component({
   selector: 'app-file',
@@ -9,20 +10,38 @@ import {GET_SYSTEM_APPS_FILE_MANAGER_DISPLAYED_FILES} from "../../config/Apps/fi
   styleUrls: ['./file.component.scss']
 })
 export class FileComponent implements OnInit {
-
   private foldersView: fileConfigDisplayedMetadata[] = GET_SYSTEM_APPS_FILE_MANAGER_DISPLAYED_FILES()
-
-  // ICON AND NAME
-
+  public declare topTxtView: { file: string, edit: string, view: string, go: string };
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.topTxtView = {
+      file: NapicuOS.get_language_words().Apps.FileManager.file,
+      edit: NapicuOS.get_language_words().Apps.FileManager.edit,
+      view: NapicuOS.get_language_words().Apps.FileManager.view,
+      go: NapicuOS.get_language_words().Apps.FileManager.go
+    }
   }
 
   get GetHomeFolders(): fileConfigDisplayedMetadata[] {
     return this.foldersView
   }
 
+  public clickFile(): void {
+
+  }
+
+  public clickEdit(): void {
+
+  }
+
+  public clickView(): void {
+
+  }
+
+  public clickGo(): void {
+
+  }
 }
--- src/app/Sys/Systems/NapicuOS/Language/langs.ts
+++ src/app/Sys/Systems/NapicuOS/Language/langs.ts
@@ -1,6 +1,7 @@
 import {lang_Calendar_cs, lang_Calendar_en, lang_Notification_cs, lang_Notification_en} from "./CalendarMenu";
 import {lang_Month_cs, lang_Month_en} from "./Months";
 import {lang_Days_cs, lang_Days_en} from "./Days";
+import {lang_FileManager_cs, lang_FileManager_en} from "./FileManager";
 
 export declare type NapicuOS_available_language = "en" | "cs";
 
@@ -14,7 +15,10 @@ export const NapicuOSLanguages = {
       Calendar: lang_Calendar_en
     },
     Months: lang_Month_en,
-    Days: lang_Days_en
+    Days: lang_Days_en,
+    Apps: {
+      FileManager: lang_FileManager_en
+    }
   },
 
   cs: {
@@ -26,6 +30,9 @@ export const NapicuOSLanguages = {
       Calendar: lang_Calendar_cs
     },
     Months: lang_Month_cs,
-    Days: lang_Days_cs
+    Days: lang_Days_cs,
+    Apps: {
+      FileManager: lang_FileManager_cs
+    }
   }
 }
