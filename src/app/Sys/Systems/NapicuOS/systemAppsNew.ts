76ce67fbbcf62d81d5b6f2af265dd6eef0060e11
Marcel <numax.cz@protonmail.ch>
2022-06-19 12:06:16 +0200

    Smazán nepotřebný soubor

--- src/app/Sys/Systems/NapicuOS/systemAppsNew.ts
+++
@@ -1,6 +0,0 @@
-import {SystemAppsFileManager} from "./SystemComponents/Apps/FileManager";
-import {SystemAppsTerminal} from "./SystemComponents/Apps/Terminal";
-import {SystemAppsNotepad} from "./SystemComponents/Apps/Notepad";
-import {SystemAppsWelcome} from "./SystemComponents/Apps/Welcome";
-import {SystemAppsUserManager} from "./SystemComponents/Apps/UserManager";
-
