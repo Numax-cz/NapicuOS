5854f55ec5bb70f36c6187d6b328beb323cf7ee9
Marcel <numax.cz@protonmail.ch>
2022-07-04 18:14:12 +0200

    Přidáno score, přidány kolize

--- src/app/Sys/Systems/NapicuOS/Apps/flappy/flappy.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/flappy/flappy.component.ts
@@ -24,11 +24,15 @@ export class FlappyComponent extends NapicuEngineWindow implements OnInit, After
   @ViewChild('NapicuCanvas') declare canvas: ElementRef<HTMLCanvasElement>;
   @Input() public declare process: Process;
 
-  protected player: NapicuEngineGameObject = new NapicuEngineGameObject(100, SYSTEM_GAME_CANVAS_RESOLUTION.y/2, 100, 120, SYSTEM_IMAGES.bird);
+  protected player: NapicuEngineGameObject | null= null;
 
   protected pipes: NapicuEngineGameObject[] = [];
 
   protected override backgroundColor = "#5fc5ce";
+
+  protected score: number = 0;
+
+  protected gameStarted: boolean = false;
 
   ngOnInit(): void {
 
@@ -43,7 +47,9 @@ export class FlappyComponent extends NapicuEngineWindow implements OnInit, After
   }
 
   override onInit() {
+    this.score = 0;
     this.generatePipes();
+    this.generatePlayer()
   }
 
   override update(): void {
@@ -53,18 +59,23 @@ export class FlappyComponent extends NapicuEngineWindow implements OnInit, After
 
   override render(): void {
 
-    this.renderObject(this.player);
+    if(this.player) this.renderObject(this.player);
 
     this.pipes.forEach((pipe: NapicuEngineGameObject) => {
       this.renderObject(pipe);
     });
+
+    this.renderScore();
   }
 
+  protected generatePlayer(): void {
+    this.player = new NapicuEngineGameObject(100, SYSTEM_GAME_CANVAS_RESOLUTION.y/2, 100, 120, SYSTEM_IMAGES.bird);
+  }
 
   protected generatePipes(): void {
     for (let i = 0; i < SYSTEM_APPS_FLAPPY_PIPES_COUNT; i += 2){
       //Bottom
-      this.pipes[i] = new FlappyPipe((i + 2) * SYSTEM_APPS_FLAPPY_PIPE_VOID_SIZE , this.getPipeRandomPosition() );
+      this.pipes[i] = new FlappyPipe((i + 2) * SYSTEM_APPS_FLAPPY_PIPE_VOID_SIZE , this.getPipeRandomPosition());
       //Top
        let tp_pipe = new FlappyPipe(this.pipes[i].x, this.pipes[i].y - 1000);
        tp_pipe.rotate(180);
@@ -78,34 +89,73 @@ export class FlappyComponent extends NapicuEngineWindow implements OnInit, After
   }
 
   protected updatePipes(): void {
-
+    if(!this.gameStarted || !this.player) return;
     for(let i = 0; i < this.pipes.length; i ++){
 
       this.pipes[i].x -= SYSTEM_APPS_FLAPPY_PIPE_SPEED;
 
+      //X position
+      if(this.player.x + this.player.width/2 > this.pipes[i].x - FlappyPipe.width/2 &&  this.player.x + this.player.width/2 < this.pipes[i].x + FlappyPipe.width/2){
+        //Y position TOP - BOT
+        if((this.player.y - this.player.width/2) < this.pipes[i].y + FlappyPipe.height/2 && (this.player.y + this.player.width/2) > this.pipes[i].y - FlappyPipe.height/2){
+          this.gameOver();
+        }
+      }
+
       if(this.pipes[i].x + SYSTEM_APPS_FLAPPY_PIPE_VOID_SIZE + FlappyPipe.width/2 < 0){
-        this.pipes[i].x = this.canvas.nativeElement.width + FlappyPipe.width/2;
+
+        this.pipes.splice(0, 2);
+
+        //Bottom
+        this.pipes.push(new FlappyPipe((this.canvas.nativeElement.width + FlappyPipe.width /2)  , this.getPipeRandomPosition()))
+
+        //Top
+        let tp_pipe = new FlappyPipe(this.pipes[this.pipes.length - 1].x, this.pipes[this.pipes.length - 1].y - 1000);
+        tp_pipe.rotate(180);
+        this.pipes.push(tp_pipe);
+        this.score++;
       }
     }
-
-
   }
 
+  protected gameOver(): void {
+    this.gameStarted = false;
+    this.onInit();
+  }
 
+  protected renderScore(): void {
+    if(!this.ctx) return
+    this.ctx.fillStyle = 'white';
+    this.ctx.font = '80pt DosVGA';
+    this.ctx.strokeStyle = 'black';
+    this.ctx.textBaseline = 'top';
+    this.ctx.fillText (this.score.toString(), this.canvas.nativeElement.width/2 - 53, 50);
+    this.ctx.strokeText(this.score.toString(), this.canvas.nativeElement.width/2 - 53, 50);
+
+    this.ctx.lineWidth = 4;
+
+    this.ctx.fill();
+    this.ctx.stroke();
+  }
 
   protected updatePlayer(): void {
+    if(!this.player) return;
     this.playerGravitation();
-    if(this.keyBoard.isPressed(KeyCodes.SPACE)){
+    if (this.keyBoard.isPressed(KeyCodes.SPACE)) {
+      if (!this.gameStarted) this.gameStarted = true;
 
       //TODO CONFIG
       this.player.velocityY -= 25;
 
       this.player.rotateDelta = -0.4;
-
+    }
+    if(this.player.y  <= 0 || (this.player.y + this.player.height/2) >= this.canvas.nativeElement.height){
+        this.gameOver();
     }
   }
 
   protected playerGravitation(): void {
+    if(!this.gameStarted || !this.player) return;
     this.player.x += this.player.velocityX
     this.player.y += this.player.velocityY
     this.player.velocityY *= 0.93;
