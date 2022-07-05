45d599e9eab780126aba26fc989e5480c9b38ab9
Marcel <numax.cz@protonmail.ch>
2022-07-05 21:28:22 +0200

    Přidán model GPU

--- src/app/Bios/vm_computer.ts
+++ src/app/Bios/vm_computer.ts
@@ -1,8 +1,8 @@
 export const VM_COMPUTER_INFORMATION = {
   cpu: "AMD Celeron (tm) II B45 Processor @ 2.6 GHz (64bit)",
+  gpu: "ATI Radeon X550",
   cpu_speed: "2666MHz",
   cache_size: "8192KB",
   mem_size: "8192MB",
-  serial_number: "HRV54JFKD54FJ34FJfj333"
-
+  serial_number: "HRV54JFKD54FJ34FJfj333",
 }
