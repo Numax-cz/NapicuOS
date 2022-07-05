aceade40279bb42be4d592827db32be5f39ad54c
Marcel <numax.cz@protonmail.ch>
2022-07-05 21:34:13 +0200

    Přidány informace o zařízení

--- src/app/Sys/Systems/NapicuOS/Apps/settings/about/about.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/settings/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NapicuBios} from "../../../SystemComponents/Bios";
+import {NapicuOS} from "../../../system.napicuos";
 
 @Component({
   selector: 'app-about',
@@ -11,5 +13,24 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  get GetCpuModel(): string {
+    return NapicuBios.get_cpu_name();
+  }
+
+  get GetGpuModel(): string {
+    return NapicuBios.get_gpu_name();
+  }
+
+  get GetMemSize(): string {
+    return NapicuBios.get_mem_size();
+  }
+
+  get GetDeviceName(): string {
+    return NapicuOS.get_hostname();
+  }
+
+
 
 }
