98926df6c1bf8cfb13fc5affa790b212b6a8598b
Marcel <numax.cz@protonmail.ch>
2022-07-05 15:42:32 +0200

    Přidáno interface a konfigurace aplikace nastavení

---
+++ src/app/Sys/Systems/NapicuOS/config/Apps/Settings.ts
@@ -0,0 +1,7 @@
+import {SettingsOptionsMetadata} from "../../interface/Apps/Settings";
+
+export const SYSTEM_APPS_SETTINGS_OPTIONS: SettingsOptionsMetadata[] = [
+
+
+
+]
---
+++ src/app/Sys/Systems/NapicuOS/interface/Apps/Settings.d.ts
@@ -0,0 +1,6 @@
+import {Type} from "@angular/core";
+
+export declare interface SettingsOptionsMetadata{
+  name: string,
+  component: Type<any>
+}
