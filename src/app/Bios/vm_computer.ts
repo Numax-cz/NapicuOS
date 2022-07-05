5543ca57e48763d97dc07d1910e6cbd00887c558
Marcel <numax.cz@protonmail.ch>
2022-07-05 21:19:59 +0200

    Upraven config

---
+++ src/app/Bios/vm_computer.ts
@@ -0,0 +1,8 @@
+export const VM_COMPUTER_INFORMATION = {
+  cpu: "AMD Celeron (tm) II B45 Processor @ 2.6 GHz (64bit)",
+  cpu_speed: "2666MHz",
+  cache_size: "8192KB",
+  mem_size: "8192MB",
+  serial_number: "HRV54JFKD54FJ34FJfj333"
+
+}
