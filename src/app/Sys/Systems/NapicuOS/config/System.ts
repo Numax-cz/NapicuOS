9306a8782da5476e43bc2ece9c0f89966951202a
Marcel <numax.cz@protonmail.ch>
2022-07-06 21:58:19 +0200

    Přidána nová ikona

--- src/app/Sys/Systems/NapicuOS/config/System.ts
+++ src/app/Sys/Systems/NapicuOS/config/System.ts
@@ -81,7 +81,8 @@ export enum SYSTEM_IMAGES {
   settings = "assets/systems/NapicuOS/SystemIcons/XFD/settings.svg",
   shutdown = "assets/systems/NapicuOS/SystemIcons/XFD/shutdown.svg",
   paint = "assets/systems/NapicuOS/SystemIcons/XFD/paint.svg",
-  time = "assets/systems/NapicuOS/SystemIcons/XFD/symbols/time.svg"
+  time = "assets/systems/NapicuOS/SystemIcons/XFD/symbols/time.svg",
+  users = "assets/systems/NapicuOS/SystemIcons/XFD/symbols/users.svg"
 
 }
 
