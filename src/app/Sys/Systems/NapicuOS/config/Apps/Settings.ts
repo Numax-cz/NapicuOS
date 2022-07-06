c3a6eae54d9e54ad379850287de6db21b95f645c
Marcel <numax.cz@protonmail.ch>
2022-07-06 21:53:58 +0200

    Upraveny importy

--- src/app/Sys/Systems/NapicuOS/config/Apps/Settings.ts
+++ src/app/Sys/Systems/NapicuOS/config/Apps/Settings.ts
@@ -1,13 +1,20 @@
 import {SettingsOptionsMetadata} from "../../interface/Apps/Settings";
 import {AboutComponent} from "../../Apps/settings/about/about.component";
 import {SYSTEM_IMAGES} from "../System";
+import {TimeComponent} from "../../Apps/settings/time/time.component";
 
-export const SYSTEM_APPS_SETTINGS_OPTIONS: SettingsOptionsMetadata[] = [
+export const SYSTEM_APPS_SETTINGS_OPTIONS: SettingsOptionsMetadata[] = [ //TODO CONFIG
+  {
+    name: "Time",
+    icon: SYSTEM_IMAGES.time,
+    component: TimeComponent
+  },
   {
     name: "About",
     icon: SYSTEM_IMAGES.Info2,
     component: AboutComponent
   }
+
 
 
 ]
