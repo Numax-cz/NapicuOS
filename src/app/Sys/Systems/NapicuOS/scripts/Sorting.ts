23da55e5d1efeaab6b574090a0aa7debf5eacbdb
Marcel <numax.cz@protonmail.ch>
2022-07-18 21:41:45 +0200

    Přidána funkce pro třídění systémových souborů podle názvu

--- src/app/Sys/Systems/NapicuOS/scripts/Sorting.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/Sorting.ts
@@ -1,3 +1,20 @@
+import {SystemFile} from "../SystemComponents/File";
+
 export function ABCSortArray(array: string[]): string[]{
   return array.sort();
+}
+
+export function ABCSortSystemFiles(files: SystemFile[]): SystemFile[]{
+  return files.sort((fileA: SystemFile, fileB: SystemFile) => {
+    let fileNameA = fileA.fileName.toLowerCase();
+    let fileNameB = fileB.fileName.toLowerCase();
+
+    if (fileNameA < fileNameB) {
+      return -1;
+    }
+    if (fileNameA > fileNameB) {
+      return 1;
+    }
+    return 0;
+  });
 }
