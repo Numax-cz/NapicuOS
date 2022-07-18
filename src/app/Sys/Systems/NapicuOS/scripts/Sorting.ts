109f9decda24d84cc92fc56f655aeba64961b1e2
Marcel <numax.cz@protonmail.ch>
2022-07-18 21:44:06 +0200

    Přidána dokumentace

--- src/app/Sys/Systems/NapicuOS/scripts/Sorting.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/Sorting.ts
@@ -1,13 +1,23 @@
 import {SystemFile} from "../SystemComponents/File";
 
+/**
+ * Sorts array alphabetically
+ * @param array
+ * @constructor
+ */
 export function ABCSortArray(array: string[]): string[]{
   return array.sort();
 }
 
+/**
+ * Sorts files alphabetically
+ * @param files
+ * @constructor
+ */
 export function ABCSortSystemFiles(files: SystemFile[]): SystemFile[]{
   return files.sort((fileA: SystemFile, fileB: SystemFile) => {
-    let fileNameA = fileA.fileName.toLowerCase();
-    let fileNameB = fileB.fileName.toLowerCase();
+    let fileNameA: string = fileA.fileName.toLowerCase();
+    let fileNameB: string = fileB.fileName.toLowerCase();
 
     if (fileNameA < fileNameB) {
       return -1;
