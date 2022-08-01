a85f709029c03471d2ba6c0c454a9fb7f7d25dec
Marcel <numax.cz@protonmail.ch>
2022-08-01 09:26:33 +0200

    Opravena kontrola stringu

--- src/app/Sys/Systems/NapicuOS/scripts/ChckStringCorrection.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/ChckStringCorrection.ts
@@ -1,8 +1,8 @@
 import {SystemStateMetadata, SystemStringStateCorrection} from "../interface/System";
 
 //TODO Doc
-export function checkSystemStringLength(value: string, min: number, max: number): SystemStringStateCorrection {
-  if (value?.length < min) return SystemStateMetadata.StringTooShort;
-  if (value?.length > max) return SystemStateMetadata.StringTooLong;
+export function checkSystemStringLength(value: string | undefined, min: number, max: number): SystemStringStateCorrection {
+  if (!value || value.length < min) return SystemStateMetadata.StringTooShort;
+  if (value.length > max) return SystemStateMetadata.StringTooLong;
   return SystemStateMetadata.StringCorrect;
 }
