ca7d591f8445cf35595e4ab7eb24b5ca1f09caec
Marcel <numax.cz@protonmail.ch>
2022-08-01 09:22:13 +0200

    Opraven script

--- src/app/Sys/Systems/NapicuOS/scripts/ChckStringCorrection.ts
+++ src/app/Sys/Systems/NapicuOS/scripts/ChckStringCorrection.ts
@@ -2,7 +2,7 @@ import {SystemStateMetadata, SystemStringStateCorrection} from "../interface/Sys
 
 //TODO Doc
 export function checkSystemStringLength(value: string, min: number, max: number): SystemStringStateCorrection {
-  if (value.length < min) return SystemStateMetadata.StringTooShort;
-  if (value.length > max) return SystemStateMetadata.StringTooLong;
+  if (value?.length < min) return SystemStateMetadata.StringTooShort;
+  if (value?.length > max) return SystemStateMetadata.StringTooLong;
   return SystemStateMetadata.StringCorrect;
 }
