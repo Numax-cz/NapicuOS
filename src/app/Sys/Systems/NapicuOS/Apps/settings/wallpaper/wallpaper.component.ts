bbad872cca381965ace67c18d27b182a8a64987a
Marcel <numax.cz@protonmail.ch>
2022-08-10 20:40:43 +0200

    Přidáno zobrazování errorů

--- src/app/Sys/Systems/NapicuOS/Apps/settings/wallpaper/wallpaper.component.ts
+++ src/app/Sys/Systems/NapicuOS/Apps/settings/wallpaper/wallpaper.component.ts
@@ -14,14 +14,9 @@ export class WallpaperComponent  implements OnInit {
   public declare system_wallpapers: string[];
   public wallpaper_url: string = "";
   public interval_input: Interval = new Interval();
+  public error_input: boolean = false;
 
-  constructor() {
-
-  }
-
-
-
-
+  constructor() { }
 
   ngOnInit(): void {
     this.system_wallpapers = Object.values(SYSTEM_WALLPAPERS);
@@ -41,24 +36,29 @@ export class WallpaperComponent  implements OnInit {
   }
 
   public onChangeURL(): void {
-    this.interval_input.delay(() => {
-      imagePreloader(this.wallpaper_url).then(
-        s => {
-          this.setWallpaper(this.wallpaper_url);
-        },
-        e => {
-          //TODO IMG not found LoL
-        }
-      )
-    });
+    this.error_input = false;
+    if(this.wallpaper_url.length){
+      this.interval_input.delay(() => {
+        imagePreloader(this.wallpaper_url).then(
+          s => {
+            this.setWallpaper(this.wallpaper_url);
+          },
+          e => {
+            this.error_input = true;
+          }
+        )
+      });
+    }
   }
 
+  public setURLText(): void {
+    if(NapicuOS.get_active_user_wallpaper_index() < 0) this.wallpaper_url =  NapicuOS.get_active_user_wallpaper();
+    else this.wallpaper_url = "";
+
+    this.error_input = false;
+  }
 
   public setWallpaper(name: string): void {
     NapicuOS.set_wallpaper(name);
   }
-
-
-
-
 }
