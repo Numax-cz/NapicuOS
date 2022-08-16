53b6f7110ff1a5398c00867a376dd8b2e478e1d4
Marcel <numax.cz@protonmail.ch>
2022-08-16 11:07:53 +0200

    Přidán ControlValueAccessor

--- src/app/Sys/Systems/NapicuOS/template/counter-input/counter-input.component.ts
+++ src/app/Sys/Systems/NapicuOS/template/counter-input/counter-input.component.ts
@@ -1,28 +1,58 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-counter-input',
   templateUrl: './counter-input.component.html',
-  styleUrls: ['./counter-input.component.scss']
+  styleUrls: ['./counter-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CounterInputComponent),
+    multi: true
+  }]
 })
-export class CounterInputComponent implements OnInit {
-
+export class CounterInputComponent implements OnInit, ControlValueAccessor  {
+  val: number = 0;
+  onChange: any = () => {}
+  onTouch: any = () => {}
 
   @Input() public min: number = 0;
   @Input() public max: number = 100;
-  @Input() public value: number = 0;
-  @Input() public declare onUpdate: () => void;
+
+
+
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+ }
 
   public clickMinus(): void {
-    if(this.value > this.min) this.value--;
+    if(this.val > this.min) this.value = this.val - 1;
   }
 
   public clickPlus(): void {
-    if(this.value < this.max) this.value++;
+    if(this.val < this.max) this.value = this.val + 1;
+  }
+
+
+  set value(val: number){
+    if( val !== undefined && this.val !== val){
+      this.val = val
+      this.onChange(val)
+      this.onTouch(val)
+    }
+  }
+
+  writeValue(value: any){
+    this.value = value
+  }
+
+  registerOnChange(fn: any){
+    this.onChange = fn
+  }
+
+  registerOnTouched(fn: any){
+    this.onTouch = fn
   }
 }
