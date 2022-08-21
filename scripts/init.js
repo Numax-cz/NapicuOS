cb08d4160033cc07ed9f11a9aef130fa2625a1b7
Marcel <numax.cz@protonmail.ch>
2022-08-21 15:55:37 +0200

    Přidány a upraveny scripty

---
+++ scripts/init.js
@@ -0,0 +1,17 @@
+/**
+ * @author Numax
+ * @version 1.0.1
+ */
+
+const fs = require('fs');
+const {exec} = require('child_process');
+
+
+
+function Run() {
+
+
+
+}
+
+Run()
---
+++ scripts/server.js
@@ -0,0 +1,50 @@
+/**
+ * @author Numax
+ * @version 1.0.0
+ *
+ * Script to easily start spring boot server
+ */
+
+const fs = require('fs');
+const {exec} = require('child_process');
+const workingDir = "./Server"
+
+let Profile;
+process.argv.forEach(function (val, index) {
+  switch (index) {
+    case 2:
+      Profile = val;
+      break;
+    default:
+      break;
+  }
+});
+
+function Run() {
+  if (Profile){
+
+    let i = exec(".\\mvnw spring-boot:run -Dspring-boot.run.profiles=dev-local -f pom.xml",{ cwd: workingDir} , (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (error !== null) {
+        console.error(error);
+      }
+    });
+
+    i.stdout.on('data', function(data) {
+      console.log(data);
+    });
+
+
+  }else {
+    console.error(`
+             Please enter parameters!
+    `);
+    console.error(`
+        npm server <spring-boot-profile>
+    `);
+    process.exit(0);
+  }
+}
+
+Run();
