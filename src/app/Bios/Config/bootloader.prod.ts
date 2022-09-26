5a2211ab9eec39329892260d76247ecc3d0d4ca2
Marcel <numax.cz@protonmail.ch>
2022-09-26 17:22:18 +0200

    Formátovány konfigurace

--- src/app/Bios/Config/bootloader.prod.ts
+++ src/app/Bios/Config/bootloader.prod.ts
@@ -1,21 +1,11 @@
-// export const grub_time_out: number = 500;
-// export const grub_time_in: number = 1050;
-// export const system_enter = 1250;
-//TODO Doc
-
-
 export const boot_configuration = {
-
   grub_time_out: 500,
   grub_time_in: 1050,
   system_enter: 1250,
-  bootloader_blackscreen_timeout: 1350, //todo 1350
-
-
-  startTimeIn: 2055, //TODO 2055
-  startTimeOut: 1055, //TODO 1055
-
-  exitTime: 2500, //TODO 2500
+  bootloader_blackscreen_timeout: 1350,
+  startTimeIn: 2055,
+  startTimeOut: 1055,
+  exitTime: 2500,
 
   production: true,
 }
