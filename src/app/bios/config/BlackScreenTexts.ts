5665ab8ceb4912b942f98ad25115d0afcb4c6d3b
Marcel <numax.cz@protonmail.ch>
2022-10-08 17:47:38 +0200

    Upraveny bootovací texty

--- src/app/bios/config/BlackScreenTexts.ts
+++ src/app/bios/config/BlackScreenTexts.ts
@@ -1,8 +1,7 @@
 export const NoBootDevice: string[] = [
   'No boot device available.',
   'Current boot mode is set to BIOS.',
-  'Please ensure compatible bootable media is available.',
-  'Use the system setup program to change the boot mode as needed.',
+  'Please reboot and select proper Boot device.',
   '',
   'Press F1 to reboot device.',
 ];
