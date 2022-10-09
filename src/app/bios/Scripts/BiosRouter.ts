f0aa1f1194fee1ea8a257522970ee2cd1ddf62f0
Marcel <numax.cz@protonmail.ch>
2022-10-09 11:02:37 +0200

    Přidán skip location change

--- src/app/bios/Scripts/BiosRouter.ts
+++ src/app/bios/Scripts/BiosRouter.ts
@@ -5,5 +5,5 @@ import {BootComponent} from '../components/boot/boot.component';
  * @param Path - URL Path
  */
 export function Navigate(Path: string) {
-  BootComponent.NavigateRouter.navigate([Path], { skipLocationChange: true });
+  BootComponent.NavigateRouter.navigate([Path], { skipLocationChange: false });
 }
