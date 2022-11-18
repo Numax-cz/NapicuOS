028ebbb72acb21a850ef356f6769845ff793757e
Marcel <numax.cz@protonmail.ch>
2022-11-18 18:46:14 +0100

    Opravena chyba zavaděče

--- src/app/bios/Scripts/exit/Boot.ts
+++ src/app/bios/Scripts/exit/Boot.ts
@@ -2,10 +2,11 @@ import {BootComponent} from 'src/app/bios/components/boot/boot.component';
 import {BootLoader} from '../bootloader/BootLoader';
 import {Loading} from '../LoadingAnimations';
 import {BiosComponent} from "../../components/bios/bios.component";
+import {BiosSettings} from "../../ToolSettings";
 
 export function Boot(): void {
   BootComponent.EnterBios = false;
   Loading('/blackscreen', 1050, 200);
-  var bootLoader = new BootLoader(BiosComponent.BiosMenuSavePoint);
+  const bootLoader = new BootLoader(BiosComponent.BiosMenuSavePoint || BiosSettings);
   bootLoader.check();
 }
